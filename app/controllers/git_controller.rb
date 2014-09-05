a6696bdbdd659095771151ef66881fcd7d9662b5
Russell White <rarctem@gmail.com>
2014-09-05 13:41:33 -0600

    Add option to specify command in URL.

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -1,7 +1,10 @@
 class GitController < ApplicationController
   def index
+    #handle params
+    @command_name = params[:command]
+
     @tagline = 'in-dev-forever'
-    @command_name = %x(python python/generate_info.py git_command)
+    @command_name = %x(python python/generate_info.py git_command) unless @command_name
     @command_desc = %x(python python/generate_info.py git_short_desc #{@command_name})
     @command_synopsis = %x(python python/generate_info.py git_synopsis #{@command_name})
     @command_synopsis = @command_synopsis.scan(/.{1,60}]/).join("\n")
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'git#index'
+  get '/docs/:command', to: 'git#index'
 end
